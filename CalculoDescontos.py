5af3c7a61a1deb18c70f202393611f88d653eb12
Marcota26 <marciojunior0105@gmail.com>
2022-05-25 14:24:05 -0300

    alteração de função try por função if

--- CalculoDescontos.py
+++ CalculoDescontos.py
@@ -1,36 +1,36 @@
 while True:
     valor_hora = input('Quanto você recebe por hora?: ')
-    try:
-        valor_hora = float(valor_hora)
-        print(type(valor_hora))
-        break
-    except:
+    if not valor_hora.isdecimal():
         print('Valor invalido, tente novamente.')
         continue
-
+    else:
+        valor_hora = float(valor_hora)
+        print('Valor válido.')
+        break
 while True:
     horas_trabalhadas = input('Quantas horas você trabalha por mês?: ')
-    try:
-        horas_trabalhadas = int(horas_trabalhadas)
-        print(type(horas_trabalhadas))
-        break
-    except:
+    if not horas_trabalhadas.isnumeric():
         print('Valor invalido, tente novamente.')
         continue
+    else:
+        horas_trabalhadas = int(horas_trabalhadas)
+        print('Valor válido.')
+        break
+  
 
 salario_Bruto = valor_hora * horas_trabalhadas
 
-print(f'seu salario bruto é de: {salario_Bruto} reais por mês.')
+print(f'seu salario bruto é de: R${salario_Bruto} por mês.')
 
 valor_IR = round((salario_Bruto * 0.11), 2)
-print(f'Será descontado de seu salário referente ao IR: {valor_IR}')
+print(f'Será descontado de seu salário referente ao IR: R${valor_IR}')
 
-valor_INSS = round((salario_Bruto * 0.08), 2)
-print(f'Será descontado de seu salário referente ao INSS: {valor_INSS}')
+valor_INSS = round((salario_Bruto * 0.08), 3)
+print(f'Será descontado de seu salário referente ao INSS: R${valor_INSS}')
 
 valor_Sindicato = round((salario_Bruto * 0.05), 2)
-print(f'Será descontado de seu salário referente ao Sindicato: {valor_Sindicato}')
+print(f'Será descontado de seu salário referente ao Sindicato: R${valor_Sindicato}')
 
 salario_Liquido = round((salario_Bruto - valor_IR - valor_INSS - valor_Sindicato), 2)
-print(f'Salario liquido mensal: {salario_Liquido}')
+print(f'Salario liquido mensal: R${salario_Liquido}')
 
